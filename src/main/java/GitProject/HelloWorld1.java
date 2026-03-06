46ca245e90841a98cfc25d3153b6c91cfa8e7acb
Nora Umali <Nakitay84@gmail.com>
2026-03-06 13:34:35 -0700

    Add additional print statements to HelloWorld1
    updates

--- src/main/java/GitProject/HelloWorld1.java
+++ src/main/java/GitProject/HelloWorld1.java
@@ -3,6 +3,9 @@ package GitProject;
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+           System.out.println("Some changes is added");
+           System.out.println("Hello World, Hi NArgiza 
+                              ");
     }
 
 
